b0773604f317537edf3aa3265f28d26585b3ea52
Mohamed Elattar <maaelattar@gmail.com>
2025-01-30 05:56:15 +0200

    uncomment migrations to fix Vercel deployment

--- app/migrations/002-add-get-invoice-status-function/route.ts
+++ app/migrations/002-add-get-invoice-status-function/route.ts
@@ -1,16 +1,16 @@
-// import runMigration from "../runMigration";
+import runMigration from '../runMigration';
 
-// export async function GET() {
-//   return runMigration("002-add-get-invoice-status-function", async (client) => {
-//     await client.sql`CREATE OR REPLACE FUNCTION get_invoice_status(status VARCHAR, due_date DATE)
-//         RETURNS VARCHAR AS $$
-//         SELECT CASE WHEN status = 'pending' AND due_date < CURRENT_DATE THEN 'overdue'
-//         ELSE status
-//         END
-//         $$ LANGUAGE SQL STABLE;
-//         `;
-//   });
-// }
+export async function GET() {
+  return runMigration('002-add-get-invoice-status-function', async (client) => {
+    await client.sql`CREATE OR REPLACE FUNCTION get_invoice_status(status VARCHAR, due_date DATE)
+        RETURNS VARCHAR AS $$
+        SELECT CASE WHEN status = 'pending' AND due_date < CURRENT_DATE THEN 'overdue'
+        ELSE status
+        END
+        $$ LANGUAGE SQL STABLE;
+        `;
+  });
+}
 
 // TODO
 // DELETE
--- app/migrations/003-add-invoice-status-logs-table/route.ts
+++ app/migrations/003-add-invoice-status-logs-table/route.ts
@@ -1,24 +1,24 @@
-// import runMigration from '../runMigration';
+import runMigration from '../runMigration';
 
-// export async function GET() {
-//   return runMigration('003-add-invoice-status-logs-table', async (client) => {
-//     await client.sql`CREATE TABLE IF NOT EXISTS invoice_status_logs
-//     (
-//     id UUID DEFAULT uuid_generate_v4(),
-//     invoice_id UUID NOT NULL,
-//     user_email VARCHAR(255) NOT NULL,
-//     invoice_old_status VARCHAR(255) NOT NULL,
-//     invoice_new_status VARCHAR(255) NOT NULL,
-//     log_status VARCHAR(255) NOT NULL DEFAULT 'active',
-//     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+export async function GET() {
+  return runMigration('003-add-invoice-status-logs-table', async (client) => {
+    await client.sql`CREATE TABLE IF NOT EXISTS invoice_status_logs
+    (
+    id UUID DEFAULT uuid_generate_v4(),
+    invoice_id UUID NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    invoice_old_status VARCHAR(255) NOT NULL,
+    invoice_new_status VARCHAR(255) NOT NULL,
+    log_status VARCHAR(255) NOT NULL DEFAULT 'active',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-//     CONSTRAINT fk_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON DELETE CASCADE
-//   )
-// `;
+    CONSTRAINT fk_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON DELETE CASCADE
+  )
+`;
 
-//     await client.sql`CREATE INDEX idx_invoice_status_logs_invoice_id ON invoices(id)`;
-//   });
-// }
+    await client.sql`CREATE INDEX idx_invoice_status_logs_invoice_id ON invoices(id)`;
+  });
+}
 
 // TODO
 // REMOVE
